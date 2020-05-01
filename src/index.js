3194e85916ece031f70353852ef358ecac7a513c
IgNog93 <igorcmnog@gmail.com>
2020-05-01 05:24:52 -0300

    fix path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './Home';
+import Home from './pages/home/Home';
 
 ReactDOM.render(
   <Home />,
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import NavMenu from '../../molecules/NavigationMenu/Navigation'
+import SectionOne from '../../molecules/section-one/Section-one'
+import SectionTwo from '../../molecules/section-two/Section-two'
+import SectionThree from '../../molecules/section-three/Section-three'
+import SectionFour from '../../molecules/section-four/Section-four'
+import SectionFive from '../../molecules/section-five/Section-five'
+import SectionSix from '../../molecules/section-six/Section-six'
+import SectionSeven from '../../molecules/section-seven/Section-seven'
+import Footer from '../../molecules/footer/Footer'
+
 import './Home.scss'
-import navMenu from '../../molecules/NavigationMenu/Navigation'
 
-const Home = () => (
-  <div>
-    <navMenu/>
-  </div>
-)
+function Home() {
+  return (
+    <>
+    <NavMenu />
+    <SectionOne />
+    <SectionTwo />
+    <SectionThree />
+    <SectionFour />
+    <SectionFive />
+    <SectionSix />
+    <SectionSeven />
+    <Footer />
+    </>
+  )
+}
 
-export default Home
+export default Home;
