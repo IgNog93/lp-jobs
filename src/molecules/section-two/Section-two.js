3c04ac049969ace01ae32a656380c731fc693752
Igor <igorcmnog@gmail.com>
2020-05-15 14:01:14 -0300

    fix section-one animation

--- src/molecules/section-two/Section-two.js
+++ src/molecules/section-two/Section-two.js
@@ -6,13 +6,13 @@ class SectionTwo extends Component {
         return (
             <section id='section-two'>
                 <div className='left-content'>
-                <ScrollAnimation animateIn="slideInLeft" animateOut="slideOutLeft" duration='1'>
+                <ScrollAnimation animateIn="slideInLeft" duration='1'>
                     <p className='title'>Somos a SAMI</p>
                     <p className='text'>Uma startup formada de pessoas com o sonho grande de tornar a Saúde descomplicada, humana e eficaz.</p>
                 </ScrollAnimation>
                 </div>
                 <div className='right-content'>
-                <ScrollAnimation animateIn="slideInRight" animateOut="slideOutRight" duration='1'>
+                <ScrollAnimation animateIn="slideInRight" duration='1'>
                     <p className='text'>Estamos reunindo as melhores práticas da Medicina,
                     <br/> repensando conceitos ultrapassados e investindo em bastante
                     <br/> tecnologia. E com muita, mas muita humildade e humanidade,
